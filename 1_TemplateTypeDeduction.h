d7f0800496dace6e80034dba5623065d1ca16d3c
Jimmy Haviland <jhaviland09@gmail.com>
2025-03-14 14:21:31 -0500

    fixed bugs, more examples

--- 1_TemplateTypeDeduction.h
+++ 1_TemplateTypeDeduction.h
@@ -18,65 +18,80 @@ namespace item_1 {
 		std::cout << std::endl;
 	}
 
-	// lambda to print type info
-	// TODO: what is the actual type? use auto for now
-	// don't get confused with this template or arg types,
-	//	the point is to print types of the other template functions
-	template<typename T>
-	auto typeInfoToStr = [](const char* sender_info, const T& param)
+	template<typename Type>
+	string GetTypeInfo()
 	{
-		string type_const_str = std::is_const<T>::value ? "const " : "";
-		string type_ref_str = std::is_reference<T>::value ? "&" : "";
-		string type_ptr_str = std::is_pointer<T>::value ? "*" : "";
-		string type_type = typeid(T).name();
+		// remove ref to check for const, and typeid ignores ref/const anyway
+		using BaseType = typename std::remove_reference<Type>::type;
+		string type_const_str = std::is_const<BaseType>::value ? "const " : "";
 
-		string type_info = type_const_str + type_ptr_str + type_type + type_ref_str;
+		string ref_or_ptr{ };
+		if (std::is_lvalue_reference<Type>::value)
+		{
+			ref_or_ptr = "&";
+		}
+		else if (std::is_rvalue_reference<Type>::value)
+		{
+			ref_or_ptr = "&&";
+		}
+		else if (std::is_pointer<Type>::value)
+		{
+			//ref_or_ptr = "*";
+			// my compiler adds an asterisk
+		}
 
-		// need to remove reference to check if const, for whatever type param may be
-		using ParamType = typename std::remove_reference<decltype(param)>::type;
-		string param_const_str = std::is_const<ParamType>::value ? "const " : "";
-		string param_ref_str = std::is_reference<decltype(param)>::value ? "&" : "";
-		string param_ptr_str = std::is_pointer<decltype(param)>::value ? "*" : "";
-		string param_type = typeid(param).name();
-
-		string param_info = param_const_str + param_ptr_str + param_type + param_ref_str;
-
-		std::cout << sender_info << ": T is " << type_info << ", param is " << param_info << std::endl;
-	};
+		string type_type = typeid(BaseType).name();
+		
+		return type_const_str + type_type + ref_or_ptr;
+	}
 
 	template<typename T>
 	void f_ref(T& param)
 	{
 		// T is deduced to whatever is passed, regardless of ref/not ref, const-ness is maintained
-		typeInfoToStr<T>("f_ref", param);
+
+		std::cout << "f_ref: T is " << GetTypeInfo<T>() << ", param is " << GetTypeInfo<decltype(param)>() << std::endl;
+
 	}
 
 	template<typename T>
 	void f_const_ref(const T& param)
 	{
 		// T is a const ref, so no longer need to deduce const-ness
-		// T is whatever type is pased, param will be const ref to that type
-		typeInfoToStr<T>("f_const_ref", param);
+		// T is whatever type is passed, param will be const ref to that type
+
+		std::cout << "f_const_ref: T is " << GetTypeInfo<T>() << ", param is " << GetTypeInfo<decltype(param)>() << std::endl;
 	}
 
 	template<typename T>
 	void f_universal_ref(T&& param)
 	{
 		// still a reference, but handles lvalue/rvalue references
+		// && is rvalue ref
 		//	lvalue (locator value, known memory location)
 		//	rvalue (right side, return value that isn't a reference)
 		//	int x = 5; x is lvalue, 5 is rvalue
 		// if param is lvalue, T and param are lvalue
 		// if param is rvalue.....
-		typeInfoToStr<T>("f_universal_ref", param);
+
+		std::cout << "f_universal_ref: T is " << GetTypeInfo<T>() << ", param is " << GetTypeInfo<decltype(param)>() << std::endl;
+
+	}
+
+	template<typename T>
+	void f_ptr(T* param)
+	{
+		std::cout << "f_ptr: T is " << GetTypeInfo<T>() << ", param is " << GetTypeInfo<decltype(param)>() << std::endl;
 	}
 
 	template<typename T>
 	void f_by_value(T param)
 	{
 		// T is whatever type passed in the expression, ignores const/ref
-		// param is a copye of the passed expression, a new object
-		typeInfoToStr<T>("f_by_value", param);
+		// param is a copy of the passed expression, a new object
+	
+		std::cout << "f_by_value: T is " << GetTypeInfo<T>() << ", param is " << GetTypeInfo<decltype(param)>() << std::endl;
+
 	}
 
 	void Run()
@@ -101,10 +116,51 @@ namespace item_1 {
 		f_universal_ref(123);	// T is int, ParamType is int&&
 		NewLine();
 
-		f_by_value(x);		// T is int&, ParamType is int&
-		f_by_value(cx);		// T is const int&, ParamType is const int&
-		f_by_value(rx);		// T is const int&, ParamType is const int&
-		f_by_value(123);	// T is int, ParamType is int&&
+		int* px = &x;
+		const int* cpx = &x;
+		const int* cpcx = &cx;
+
+		f_ptr(&x);				// T is int, param is int*
+		f_ptr(&cx);
+		f_ptr(&rx);
+
+		f_ptr(px);
+		f_ptr(cpx);				// T is const int, param is const int*
+		f_ptr(cpcx);
+		NewLine();
+
+		f_by_value(x);		// both int
+		f_by_value(cx);		// both int
+		f_by_value(rx);		// both int
+		f_by_value(123);	// both int
+		NewLine();
+
+		// arrays
+		char letters[] = { 'h', 'e', 'l', 'l', 'o', '!' };
+		const char* hello = "Hello";
+		f_by_value(hello);
+		f_by_value(letters);
+
+		f_ref(hello);
+		f_ref(letters);
+
+		f_const_ref(hello);
+		f_const_ref(letters);
+
+		f_universal_ref(hello);
+		f_universal_ref(letters);
+		f_universal_ref("what's up?");
+
+		f_ptr(hello);
+		f_ptr(letters);
+		NewLine();
+
+		// functions
+		f_ref(NewLine);
+		f_const_ref(NewLine);
+		f_universal_ref(NewLine);
+		f_by_value(NewLine);
+
 		NewLine();
 	}
 
